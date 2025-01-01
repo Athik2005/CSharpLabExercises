19291dcf167940d5828510642a43b2c0664e39fd
Athik2005 <mohamedathikr.22msc@kongu.edu>
2025-01-01 13:33:56 +0530

    Final Program of Ex2

--- ConsoleApp1/Ex2_4.cs
+++ ConsoleApp1/Ex2_4.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace C_Sharp
 {
@@ -12,7 +14,13 @@ namespace C_Sharp
         abstract public void remove(Ex2_4 item);
         abstract public void search(Ex2_4 item);
         abstract public void update(Ex2_4 item);
+        abstract public void GetProductDetails();
 
+    }
+    interface ISellable
+    {
+        public void SellItem();
+        public void RefundItem();
     }
     public class Ex2_4()
     {
@@ -20,7 +28,16 @@ namespace C_Sharp
         private String category;
         private String stock;
         private string ProductID;
+        private String ExpiryDate = "None";
         private int StockQuantity;
+        public string getExpiryDate()
+        {
+            return ExpiryDate;
+        }
+        public void setExpiryDate(String ExpiryDate)
+        {
+            this.ExpiryDate = ExpiryDate;
+        }
         public String getName()
         {
             return name;
@@ -63,40 +80,63 @@ namespace C_Sharp
         }
         public override string ToString()
         {
-            return "Name : "+name+"\nCategory : "+category+"\nStock Quantity : "+StockQuantity+"\nProductID : "+ProductID;
+            if (ExpiryDate != null)
+                return "Name : " + name + "\nCategory : " + category + "\nStock Quantity : " + StockQuantity + "\nProductID : " + ProductID + "\nExpiry Date :" + ExpiryDate;
+            return "Name : " + name + "\nCategory : " + category + "\nStock Quantity : " + StockQuantity + "\nProductID : " + ProductID;
         }
     }
-    public class InventoryItems : Item
+    public class InventoryItems : Item, ISellable
     {
         public List<Ex2_4> firstlist = new List<Ex2_4>();
         public string ProductID;
         public int StockQuantity;
+        public string exp = "N";
+        List<String> prnames = new List<string>();
+        List<String> cancelled = new List<string>();
+        bool flag = false;
+        private PerishableItem perishableItemList = new PerishableItem();
+        private NonPerishableItem nonPerishableItemList = new NonPerishableItem();
         public override void add(Ex2_4 item)
         {
-            Console.Write("Enter The Quantity of Product:");
+            Console.Write("Enter The Quantity of Product: ");
             StockQuantity = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Y if your product has Expiry Date: ");
+            exp = Console.ReadLine();
+
+            if (exp != "N")
+            {
+                item.setExpiryDate(Console.ReadLine());
+            }
+            else
+            {
+                Console.WriteLine("So I hope your product doesn't have Expiry date!\n");
+            }
+
             item.setStockQuantity(StockQuantity);
             item.setProductID(setProductId(item.getName(), item.getCategory()));
             firstlist.Add(item);
-            Console.WriteLine("Product has added successfully!");
+
+            Console.WriteLine("Product has been added successfully!");
         }
         public static string setProductId(string name, string category)
         {
             return name + "_" + category + " my product!";
         }
-        public void viewProducts()
+
+        public override void GetProductDetails()
         {
             if (firstlist.Count > 0)
             {
                 Console.WriteLine("\nList of Products!\n");
-                for (int i = 0; i < firstlist.Count; i++)
+                foreach (var product in firstlist)
                 {
-                    Console.WriteLine(firstlist[i]);
+                    Console.WriteLine(product);
                 }
             }
             else
             {
-                Console.WriteLine("No Products Bhaiya!");
+                Console.WriteLine("No Products Available.");
             }
         }
         public void viewProductsName()
@@ -104,100 +144,241 @@ namespace C_Sharp
             if (firstlist.Count > 0)
             {
                 Console.WriteLine("\nList of Products!\n");
-                for (int i = 0; i < firstlist.Count; i++)
+                for(int i = 0; i < firstlist.Count; i++)
                 {
                     Console.WriteLine(firstlist[i].getName());
                 }
             }
             else
             {
-                Console.WriteLine("No Products Bhaiya!");
+                Console.WriteLine("No Products Available.");
             }
         }
         public override void remove(Ex2_4 item)
         {
-            String name = Console.ReadLine();
-            bool flag = false;
+            Console.Write("Enter the Product Name to Remove: ");
+            string name = Console.ReadLine();
+            bool found = false;
+
             for (int i = 0; i < firstlist.Count; i++)
             {
-                if (firstlist[i].getName().Equals(name))
+                if (firstlist[i].getName().Equals(name, StringComparison.OrdinalIgnoreCase))
                 {
                     firstlist.RemoveAt(i);
-                    flag = true;
+                    found = true;
+                    break;
                 }
             }
-            if (!flag)
-            {
-                Console.WriteLine("Product not found!");
-            }
-        }
-        public override void search(Ex2_4 item)
-        {
-            String name = Console.ReadLine();
-            bool flag = false;
-            int quantity = 0;
-            for (int i = 0; i < firstlist.Count; i++)
-            {
-                if (firstlist[i].getName().Equals(name))
-                {
-                    flag = true;
-                    quantity = firstlist[i].getStockQuantity();
-                }
-            }
-            if (!flag)
+
+            if (!found)
             {
                 Console.WriteLine("Product not found!");
             }
             else
             {
-                Console.WriteLine("Product Exists & It's Quantity is " + quantity);
+                Console.WriteLine("Product removed successfully.");
             }
         }
-        public override void update(Ex2_4 item)
+        public override void search(Ex2_4 item)
         {
-            String name = Console.ReadLine();
-            bool flag = false;
+            Console.Write("Enter the Product Name to Search: ");
+            string name = Console.ReadLine();
+            bool found = false;
+            int quantity = 0;
+
             for (int i = 0; i < firstlist.Count; i++)
             {
-                if (firstlist[i].getName().Equals(name))
+                if (firstlist[i].getName().Equals(name, StringComparison.OrdinalIgnoreCase))
                 {
-                    flag = true;
+                    found = true;
+                    quantity = firstlist[i].getStockQuantity();
+                }
+            }
+
+            if (!found)
+            {
+                Console.WriteLine("Product not found!");
+            }
+            else
+            {
+                Console.WriteLine("Product Exists & Its Quantity is " + quantity);
+            }
+        }
+
+        public override void update(Ex2_4 item)
+        {
+            Console.Write("Enter the Product Name to Update: ");
+            string name = Console.ReadLine();
+            bool found = false;
+
+            for (int i = 0; i < firstlist.Count; i++)
+            {
+                if (firstlist[i].getName().Equals(name, StringComparison.OrdinalIgnoreCase))
+                {
+                    found = true;
+                    Console.Write("1. Update Product Name\n2. Update Quantity\n3. Update Category\n\nEnter a valid option: ");
                     int opt = int.Parse(Console.ReadLine());
-                    Console.Write("1.Update Product Name\n2.Update the Quanity\n3.Update the Category\n\nEnter a valid option:");
+
                     if (opt == 1)
                     {
-                        Console.WriteLine("Enter the New Product Name : ");
-                        Console.WriteLine("Old Product Name :" + firstlist[i].getName());
-                        Console.Write("New Product Name :");
+                        Console.WriteLine("Old Product Name: " + firstlist[i].getName());
+                        Console.Write("Enter New Product Name: ");
                         firstlist[i].setName(Console.ReadLine());
                     }
                     else if (opt == 2)
                     {
-                        Console.WriteLine("Enter the New Product Quanity : ");
-                        Console.WriteLine("Old Product Quanity :" + firstlist[i].getStockQuantity());
-                        Console.Write("New Product Quantity :");
+                        Console.WriteLine("Old Quantity: " + firstlist[i].getStockQuantity());
+                        Console.Write("Enter New Quantity: ");
                         firstlist[i].setStockQuantity(int.Parse(Console.ReadLine()));
                     }
                     else if (opt == 3)
                     {
-                        Console.WriteLine("Enter the New Product Category : ");
-                        Console.WriteLine("Old Product Quanity :" + firstlist[i].getCategory());
-                        Console.Write("New Product Category :");
-                        firstlist[i].setCategory(Console.ReadLine()); 
+                        Console.WriteLine("Old Category: " + firstlist[i].getCategory());
+                        Console.Write("Enter New Category: ");
+                        firstlist[i].setCategory(Console.ReadLine());
                     }
                     else
                     {
                         Console.WriteLine("Invalid Option!");
                     }
+                    break;
                 }
             }
-            if (!flag)
+
+            if (!found)
             {
                 Console.WriteLine("Product not found!");
             }
             else
             {
-                Console.WriteLine("Product Updated Successfully");
+                Console.WriteLine("Product Updated Successfully!");
+            }
+        }
+        public void classifyProducts()
+        {
+            foreach (var item in firstlist)
+            {
+                if (item.getExpiryDate() != "None" && item.getExpiryDate() != null)
+                {
+                    perishableItemList.add(item);
+                }
+                else
+                {
+                    nonPerishableItemList.add(item);
+                }
+            }
+        }
+        public void RefundItem()
+        {
+            Console.Write("Enter the Product Name : ");
+            string pname = Console.ReadLine();
+            bool flag = false;
+            for (int i = 0; i < firstlist.Count; i++)
+            {
+                if (firstlist[i].getName() == pname)
+                {
+                    Console.WriteLine("Refund Quantity for " + pname + " : ");
+                    int qty = int.Parse(Console.ReadLine());
+                    if (qty > firstlist[i].getStockQuantity())
+                    {
+                        flag = true;
+                        Console.WriteLine("Cant Refund in Negative Numbers!");
+                    }
+                    if (!flag)
+                    {
+                        firstlist[i].setStockQuantity(firstlist[i].getStockQuantity() + qty);
+                        cancelled.Add(firstlist[i].getName());
+                        Console.WriteLine("Replace made successfully!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No Product Found for " + pname + " !");
+                }
+            }
+        }
+        public void SellItem()
+        {
+            Console.Write("Enter the Product Name : ");
+            string pname = Console.ReadLine();
+            List<Ex2_4> data = firstlist;
+            Console.WriteLine("Searching for " + pname + "!");
+            for (int i = 0; i < data.Count; i++)
+            {
+                if (data[i].getName() == pname)
+                {
+                    Console.WriteLine("Enter the Quantity for " + pname + " : ");
+                    int qty = int.Parse(Console.ReadLine());
+                    if (qty > data[i].getStockQuantity() || qty < 0)
+                    {
+                        flag = true;
+                        Console.WriteLine("Purchase is more than the Quantity of " + pname);
+                    }
+                    if (!flag)
+                    {
+                        data[i].setStockQuantity(data[i].getStockQuantity() - qty);
+                        prnames.Add(data[i].getName());
+                        Console.WriteLine("Purchase made successfully!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No Product Found for " + pname + " !");
+                }
+            }
+        }
+        public void ViewPerishableItems()
+        {
+            perishableItemList.ViewPerishItems();
+        }
+
+        public void ViewNonPerishableItems()
+        {
+            nonPerishableItemList.ViewNonPerishableItems();
+        }
+    }
+    public class PerishableItem
+    {
+        public List<Ex2_4> perishItems = new List<Ex2_4>();
+        public void add(Ex2_4 item)
+        {
+            perishItems.Add(item);
+        }
+        public void ViewPerishItems()
+        {
+            if (perishItems.Count > 0)
+            {
+                foreach (var item in perishItems)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No Perishable Items Found.");
+            }
+        }
+    }
+
+    public class NonPerishableItem
+    {
+        public List<Ex2_4> nonPerish = new List<Ex2_4>();
+        public void add(Ex2_4 item)
+        {
+            nonPerish.Add(item);
+        }
+        public void ViewNonPerishableItems()
+        {
+            if (nonPerish.Count > 0)
+            {
+                foreach (var item in nonPerish)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No Non-Perishable Items Found.");
             }
         }
     }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,19 +6,20 @@ using System.Threading.Tasks;
 
 namespace C_Sharp
 {
-    public class Sample
+    public class MAIN_CLASS
     {
         static void Main()
         {
-            Ex2_4 ex = new Ex2_4();
+            Ex2_4 ex;
             InventoryItems items = new InventoryItems();
             do
             {
-                Console.WriteLine("Welcome to My Shop\n1.Add Product\n2.Delete Product using Product Name\n3.Update Product Using Product Name\n4.View All Products\n5.Exit\n\nEnter your option:");
+                ex = new Ex2_4();
+                Console.WriteLine("\nWelcome to My Shop\nMaster Inventory\n1.Add Product\n2.Delete Product using Product Name\n3.Update Product Using Product Name\n4.View All Products\n5.View Perishable Products\n6.View Non Perishable Products\n7.Purchase Products\n8.Refund Products\n9.Exit\n\nEnter your option:");
                 int opt = int.Parse(Console.ReadLine());
                 if (opt == 1)
                 {
-                    Console.WriteLine("Yaaah you are on the right way to place order!");
+                    Console.WriteLine("Yaaah you are on the right way to add new item to the inventory!");
                     Console.Write("Enter the Product Name:");
                     ex.setName(Console.ReadLine());
                     Console.Write("Enter Category of that Product:");
@@ -32,20 +33,39 @@ namespace C_Sharp
                     items.viewProductsName();
                     Console.WriteLine("Delete your product using product name\n");
                     items.remove(ex);
-                    break;
+                    
                 }
                 else if (opt == 3)
                 {
                     items.viewProductsName();
                     Console.WriteLine("Update your product using product name\n");
                     items.update(ex);
-                    break;
+                    
                 }
                 else if (opt == 4)
                 {
-                    items.viewProducts();
+                    items.GetProductDetails();
                 }
                 else if (opt == 5)
+                {
+                    items.classifyProducts();
+                    items.ViewPerishableItems();
+
+                }
+                else if (opt == 6)
+                {
+                    items.classifyProducts();
+                    items.ViewNonPerishableItems();
+                }
+                else if (opt == 7)
+                {   
+                    items.SellItem();
+                }
+                else if (opt == 8)
+                {
+                    items.RefundItem();
+                }
+                else if (opt == 9)
                 {
                     Console.WriteLine("Thanks for Booking!");
                     break;
@@ -56,6 +76,6 @@ namespace C_Sharp
                     break;
                 }
             } while (true);
-        } 
+        }
     }
 }
